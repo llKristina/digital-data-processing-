a715ee526fa63f1250fcb5171d4b1b9c710c1776
Kristina <kristina20052020@gmail.com>
2025-10-11 08:59:22 +0300

    lab 1

--- program/lab/lab1.py
+++ program/lab/lab1.py
@@ -1,52 +1,54 @@
 import cv2
 
-#task 2
-# img = cv2.imread(r"cat.jpg")
-# cv2.namedWindow("CAT", cv2.WINDOW_NORMAL)
-# cv2.imshow("CAT", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+def task2():
+    img = cv2.imread(r"cat.jpg")
+    cv2.namedWindow("CAT", cv2.WINDOW_NORMAL)
+    cv2.imshow("CAT", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+# task2()
 
-#task 3
-# cap = cv2.VideoCapture("cat2.mp4")
-# while cap.isOpened():
-#     ret, frame = cap.read()
-#     if not(ret):
-#         break
-#     frame = cv2.resize(frame, (550, 700))
-#     cv2.imshow("cat2.mp4",frame)
-#     if cv2.waitKey(1) & 0xFF ==5:
-#         break
+def task3():
+    cap = cv2.VideoCapture("cat2.mp4")
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not(ret):
+            break
+        frame = cv2.resize(frame, (550, 700))
+        cv2.imshow("cat2.mp4",frame)
+        if cv2.waitKey(1) & 0xFF ==27:
+            break
+# task3()
 
-#task 4
-# cap = cv2.VideoCapture("cat2.mp4")
-# ok, img= cap.read()
-# w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-# h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-# fourcc= cv2.VideoWriter_fourcc(*'mp4v')
-# video_writer=cv2.VideoWriter("output.mp4",fourcc,25,(w,h))
-# while(True):
-#     ok, img = cap.read()
-#     img = cv2.resize(img, (550, 700))
-#     cv2.imshow('img',img)
-#     video_writer.write(img)
-#     if cv2.waitKey(1) & 0x77 == ord('q'):
-#         break
-# video.release()
-# cv2.destroyAllWindows()
+def task4():
+    cap = cv2.VideoCapture("cat2.mp4")
+    fourcc= cv2.VideoWriter_fourcc(*'mp4v')
+    video_writer = cv2.VideoWriter("output.mp4", fourcc, 25, (550, 700))
+    while(True):
+        ok, img = cap.read()
+        if not ok:
+            break
+        img = cv2.resize(img, (550, 700))
+        cv2.imshow('img',img)
+        video_writer.write(img)
+        if cv2.waitKey(1) & 0x77 == ord('q'):
+            break
+    cap.release()
+    video_writer.release()
+    cv2.destroyAllWindows()
+# task4()
 
-#task 5
-# img = cv2.imread(r"cat.jpg")
-# hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-# img=cv2.resize(img,(350,350))
-# hsv=cv2.resize(hsv,(350,350))
-# cv2.imshow("original",img)
-# cv2.imshow("HSV", hsv)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+def task5():
+    img = cv2.imread(r"cat.jpg")
+    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    img=cv2.resize(img,(350,350))
+    hsv=cv2.resize(hsv,(350,350))
+    cv2.imshow("original",img)
+    cv2.imshow("HSV", hsv)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-#task 6
-import cv2
+# task5()
 
 def task6():
     cap = cv2.VideoCapture(0) 
@@ -71,19 +73,13 @@ def task6():
 # task6()
 
 def task7():
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cap = cv2.VideoCapture(0)
     fps = cap.get(cv2.CAP_PROP_FPS)
-    if fps == 0:  
-        fps = 10
 
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    size = (width, height)
-
-    print(f"FPS: {fps}, SIZE: {size}")
-
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    out = cv2.VideoWriter("webcam_output.mp4", fourcc, fps, size)
+    out = cv2.VideoWriter("webcam_output.mp4", fourcc, fps, (width,height))
 
     while True:
         ret, frame = cap.read()
@@ -108,10 +104,8 @@ def task8():
         if not ret:
             break
 
-        frame = cv2.flip(frame, 1)
         h, w = frame.shape[:2]
         x, y = w // 2, h // 2
-
         b, g, r = frame[y, x]
 
         if r >= g and r >= b:
